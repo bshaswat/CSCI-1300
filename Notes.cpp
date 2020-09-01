12358d806e388d29f9946ff4cfee3cabf17b07b1
Shaswat <bhattaraishaswat@gmail.com>
2020-08-31 19:57:29 -0600

    First commit

---
+++ Notes.cpp
@@ -0,0 +1,18 @@
+// Week 1
+
+#include <iostream>
+using namespace std;
+
+/*
+int main(){
+    cout << "Hello Sections 300 and 350!\n " << endl;
+    return 0;
+}
+*/
+
+int main(){
+    int initial_money = 5;
+    int num_of_stamps = 7;
+    int purchase_price_cents = num_of_stamps * 55;
+    return 0;
+}
